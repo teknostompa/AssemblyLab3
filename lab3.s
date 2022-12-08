c1d669da06bc5ba3221006e6273f49c9b3c28775
teknostompa <47609054+teknostompa@users.noreply.github.com>
2022-12-08 14:12:29 +0000

    Add files via upload

--- lab3.s
+++ lab3.s
@@ -5,10 +5,6 @@ outBuf:     .space 64
 inPos:      .quad   0
 outPos:     .quad   0
 intCount:   .quad   0
-
-
-
-
     .text
     .global putText # Tom DONE
     .global outImage # DONE
@@ -23,272 +19,214 @@ intCount:   .quad   0
     .global putChar # Tom DONE
     .global getOutPos # done
     .global setOutPos # done
-    /*Extra funktioner */
-    .global setBuffPosZero # done
-    .global setBuffPosMax # done
 
-
-
-putText: # DONE->TO TEST
-   # pushq   %RDI             # no need to push/pop, it's already secured
-    pushq   %R14            /*pushes registers to stack */
-    pushq   %R15
-    XORq    %rax, %rax      # nollställ %rax
-    movb    (%RDI), %AL     # move first char at pointer to byte register
-    movq    outPos, %r14
-    leaq    outBuf, %R14    # hämtar adressen outBuf till R14
-    addq    outPos, %R14
-    cmpb    $0, %AL
-    jne putTextChar
-    incq    outPos
-    movb    (%RDI), %AL
+putText:
+    pushq   %r14
+    leaq    outBuf, %r14    # loads outbuf to r14
+    addq    outPos, %r14    # moves outbuf to outpos
 putTextChar:
-    cmpb    $0x0, %AL       # Check if char is null
+    movb    (%rdi), %al     # move char at pointer to byte register
+    cmpb    $0, %al       # Check if char is null
     je      putTextDone     # If so, jump to end
-    movb    %AL, (%R14)     # Move char to outBuf
+    movb    %al, (%r14)     # Move char to outBuf
     incq    outPos          # Add to Out Pointer
-    incq    %R14            # move pointer to buffer forwards
-    incq    %RDI            # move pointer to string forwards
-    movb    (%RDI), %AL     # move char at pointer to byte register
-    call    checkBuffPos
+    incq    %r14            # move pointer to buffer forwards
+    incq    %rdi            # move pointer to string forwards
+    call    checkBuffPos    # check so that buffer is not full
     jmp     putTextChar     # Loop
 putTextDone:
-    popq    %R15            # pops registers from stack/retrieve registry value
-    popq    %R14
-   # movq    $0, %R14        # needed?
-   # popq    %RDI            # no need to push/pop
+    popq    %r14
     ret
 
-outImage: # Done->Tested
-/* print-> puts(buffert) */
-    pushq   %rdx           
-    pushq   %rsi           
-    pushq   %rax            
-    pushq   %rdi            # might not need
-    pushq   %r14            
-    leaq    outBuf, %R14
-    addq    outPos, %R14
-    movb    $'\n', (%R14)
-    incq    outPos
-    XORq    %rsi, %rsi
-    movq    (outPos), %rdx
-    addq    $outBuf, %rsi
-    movq    $1, %rdi
-    movq    $1, %rax
-    syscall
+outImage:
+    pushq   %r14
+    leaq    outBuf, %r14    # loads outbuf to r14
+    addq    outPos, %r14    # moves outbuf to outpos
+    movb    $0, (%r14)      # move breakline to last char
+    movq    $outBuf, %rdi   # rdi = parameter
+    call    puts            # puts(buf)
     movq    $0, outBuf      # empty outBuf
     movq    $0, outPos      # empty outBuf
     popq    %r14
-    popq    %rdi            # might not need    
-    popq    %rax            
-    popq    %rsi    
-    popq    %rdx    
     ret
 
-inImage: # DONE->MUST TEST WITH getInt, getChar..
+inImage:
     movq    $inBuf, %rdi    # rdi: IN1 = buffert lägg input-buffer i %rdi.
     movq    $64, %rsi       # rsi: IN2 = size, lägg storleken för inmätning tecken i %rsi
     movq    stdin, %rdx     # rdx: IN3 = stdin (stream)
     call    fgets           # fgets(buffert, size, stdin) = läser från tangetbordet
     movq    $0, inPos       # nollställa inData-position
-
     ret
 
 getInt:
-    pushq   %R13 
-    pushq   %R14 
-    pushq   %R15
-    leaq    inBuf, %R14 # ladda in buff
-    addq    inPos, %R14
-    XORq    %RAX, %RAX  # nollställ rax
-    movq    $10, %R15   # flytta 10 till r15 för *10 senare
+    pushq    %r13
+    pushq    %r14
+    pushq    %r15
+    leaq    inBuf, %r14     # loads inbuf to r14
+    addq    inPos, %r14     # moves inbuf to inpos
+    XORq    %rax, %rax      # nollställ rax
+    movq    $10, %r15       # flytta 10 till r15 för *10 senare
 getIntSpaceLoop:
-    movb    (%R14), %BL # flytta första karaktären till BL
-    cmpb    $' ', %BL    # Jämför med ' '
+    movb    (%r14), %bl     # flytta första karaktären till BL
+    cmpb    $' ', %bl       # Jämför med ' '
     jne continueGetInt
-    incq    %R14
-    incq    inPos
+    incq    %r14            # nästa käraktär
+    incq    inPos           # nästa käraktär
     jmp getIntSpaceLoop
 continueGetInt:
-    jne     checkNegativeInt
-    incq    %R14        # hoppa till nästa karaktär
-    incq    inPos       # hoppa till nästa karaktär
-    movb    (%R14), %BL # ladda nästa karaktär
-checkNegativeInt:
-    cmpb    $45, %BL    # Jämför med '-'
+    cmpb    $'-', %bl       # Jämför med '-'
     jne     checkPositiveInt
-    movq    $1, %R13    # sätt r13 till 1 om talet är negativt
-    incq    inPos       # hoppa till nästa karaktär
-    incq    %R14        # hoppa till nästa karaktär
+    movq    $1, %r13        # sätt r13 till 1 om talet är negativt
+    incq    inPos           # hoppa till nästa karaktär
+    incq    %r14            # hoppa till nästa karaktär
 checkPositiveInt:
-    cmpb    $43, %BL    # Jämför med '+'
+    cmpb    $'+', %bl       # Jämför med '+'
     jne     getIntDigit
-    incq    inPos       # hoppa till nästa karaktär
-    incq    %R14        # hoppa till nästa karaktär
+    incq    inPos           # hoppa till nästa karaktär
+    incq    %r14            # hoppa till nästa karaktär
 getIntDigit:
-    movb    (%R14), %BL # ladda nästa karaktär
-    cmpb    $48, %BL    # kolla om karaktären är en siffra
-    jl      getIntDone  # hoppa till slut
-    cmpb    $57, %BL    # kolla om karaktären är en siffra  
-    jg      getIntDone  # hoppa till slut
-    mulq    %R15        # gånger 10 för att det är decimalt.
-    addb    %BL, %AL    # lägg till den nya karaktären i RAX
-    subq    $48, %RAX   # 0 = 48, så vi tar bort det för att få den sanna siffran
-    incq    inPos       # hoppa till nästa karaktär
-    incq    %R14        # hoppa till nästa karaktär
-    jmp     getIntDigit # loop
+    movb    (%r14), %bl     # ladda nästa karaktär
+    cmpb    $48, %bl        # kolla om karaktären är en siffra
+    jl      getIntDone      # hoppa till slut
+    cmpb    $57, %bl        # kolla om karaktären är en siffra  
+    jg      getIntDone      # hoppa till slut
+    mulq    %r15            # gånger 10 för att det är decimalt.
+    addq    %rbx, %rax      # lägg till den nya karaktären i RAX
+    subq    $48, %rax       # 0 = 48, så vi tar bort det för att få den sanna siffran
+    incq    inPos           # hoppa till nästa karaktär
+    incq    %r14            # hoppa till nästa karaktär
+    jmp     getIntDigit     # loop
 getIntDone:
-    cmpq    $1, %R13    # kolla om nummret var negativt
-    jne returnInt       # hoppa till return
-    negq    %RAX        # om det var negativt, negera
+    cmpq    $1, %r13        # kolla om nummret var negativt
+    jne returnInt           # hoppa till return
+    negq    %rax            # om det var negativt, negera
 returnInt:
-    popq    %R15
-    popq    %R14 
-    popq   %R13 
+    popq    %r15
+    popq    %r14
+    popq    %r13
     ret
 
 getText:
-    pushq   %R14 
-    pushq   %R15 
-    pushq   %RBX 
-    XORq    %R15, %R15
-    XORq    %RAX, %RAX
-    XORq    %RBX, %RBX
-    leaq    inBuf, %R14 # ladda in buff
-    addq    inPos, %R14
-    leaq    (%rdi), %rdi
-    movb    (%R14), %BL # ladda nästa karaktär
-    cmpb    $32, %BL
-    jne     getTextLoop
-    incq    %r14
-    movb    (%R14), %BL # ladda nästa karaktär
+    pushq   %r14
+    XORq    %rax, %rax      # nollställ rax då den används som räknare
+    leaq    inBuf, %r14     # ladda in buff
+    addq    inPos, %r14     # sätt buffertposition till nuvarande
+    leaq    (%rdi), %rdi    # ladda returnbuffert till rdi
+    movb    (%r14), %bl     # ladda nästa karaktär
+    cmpb    $' ', %bl       # kolla om space före
+    jne     getTextLoop     # om inte, läser vi
+    incq    %r14            # öka inbuffer
+    movb    (%r14), %bl     # ladda nästa karaktär
 getTextLoop:
-    cmpq    $10, %RBX
+    cmpb    $0, %bl         # kolla om 0, isåfall är vi klara
     je  getTextReturn
-    movq    %RBX, (%rdi)
-    incq    %RAX
-    incq    %rdi
-    incq    %r14
-    incq    inPos
-    movb    (%R14), %BL # ladda nästa karaktär
-    cmpq    %RAX, %rsi
-    jne     getTextLoop
+    movb    %bl, (%rdi)     # flyttta karaktär till returnbuffert
+    incq    %rax            # öka rax, antal siffror som är laddade
+    incq    %rdi            # flytta returnbuffert frammåt
+    incq    %r14            # flytta inbuffert frammåt
+    incq    inPos           # öka inpos
+    movb    (%r14), %bl     # ladda nästa karaktär
+    cmpq    %rax, %rsi      # kolla om vi läst maxkäraktärer, rsi är från caller
+    jne     getTextLoop     # fortsätt
 getTextReturn:
-    movq    $'\n', (%rdi)
-    subq    %RAX, %rdi
-    decq    %rdi
-    popq    %rBX
-    popq    %r15
     popq    %r14
     ret
 
 getChar:
+    pushq   %r14
+    leaq    inBuf, %r14     # ladda in buff
+    addq    inPos, %r14     # sätt buffertposition till nuvarande
+    movq    (%r14), %rax    # flytta käraktären till return
+    incq    inPos           # öka inpos
+    popq    %r14
     ret
 
 getInPos:
-    movq    inPos, %rax
+    movq    inPos, %rax #flytta inpos till returnvärde
     ret
 
-#---------- setInPos ----------#
-setInPos: # DONE->TO TEST
-    movq    %rdi, inPos     # set buffertposition to inPos variable.
-    cmpq    $0, %rdi
+setInPos:
+    movq    %rdi, inPos     # sätt inpos till invariablel.
+    cmpq    $0, %rdi        # input min = 0
     jl      setBuffPosZero
-    cmpq    $64, %rdi
+    cmpq    $64, %rdi       # input max = 0
     jg      setBuffPosMax
     ret
-#-----------------------------#
 
-
-putInt: # On-going
-/* Register-info
-    %RDI = in number
-    
- */            
-    pushq   %R15 /*pushes registers to stack */
-    pushq   %R14
-    pushq   %R13
-    XORq    %rax, %rax
-    XORq    %R13, %R13
-    movq    $10, %r15
-    # om det är negativt
-    cmpq    $0, %rdi
-    jge     putIntNumber
-    pushq   %rdi
-    movq    outPos, %r14
-    cmpq    $-1, outPos
-    je      negativeContinue
-    decq    outPos
-negativeContinue:
-    movq    $45, %rdi
-    incq    outPos
-    call    putChar
-    popq    %rdi
-    negq    %rdi
-    # om det är negativt
-putIntNumber:   #resten är posititvt
-    XORq    %rdx, %rdx
-    movq    %rdi, %rax
-    divq    %r15    # siffran hamnar i rdx
-    movq    %rax, %rdi  # talet delat på 10 är i rax
-    pushq   %rdx
-    incq    %r13
-    cmpq    $0, %rdi
+putInt:
+    pushq   %r13
+    pushq   %r15
+    XORq    %r13, %r13      # nollställ r13 för att den räknar senare
+    movq    $10, %r15       # sätt nämnaren i förväg
+    cmpq    $0, %rdi        # kolla om nummret är negativt
+    jge     putIntNumber    # om det är positivt så kollar vi nummer direkt
+    pushq   %rdi            # pusha rdi då putchar förstör
+    cmpq    $0, outPos      # kolla om + skrevs ut innan
+    jle     negativeContinue
+    decq    outPos          # om outpos är större än 0 så ligger + före\
+negativeContinue:           # så vi minskar med 1 för att skriva över
+    movq    $45, %rdi       # flytta '-' till rdi
+    incq    outPos          # öka outpos igen
+    call    putChar         # kalla putChar
+    popq    %rdi            # poppa rdi dp putchar förstör
+    negq    %rdi            # negera talet så att vi nu har positivt
+putIntNumber:
+    XORq    %rdx, %rdx      # divq använder rdx:rax som 128bit tal
+    movq    %rdi, %rax      # täljaren i rax
+    divq    %r15            # nämnaren i r15 
+    movq    %rax, %rdi      # kvot hamnar i rax
+    pushq   %rdx            # rest hamnar i rdx, pusha för att vi får baklänges
+    incq    %r13            # räknare för antal siffror
+    cmpq    $0, %rdi        # om rdi är 0 behöver vi inte dela längre
     jg  putIntNumber
 putIntFinal:
-    popq    %rdi
-    addq    $48, %rdi
-    call    putChar
-    decq    %r13
-    cmpq    $0, %r13
-    jne putIntFinal
+    popq    %rdi            # poppa ut tal i rätt ordning
+    addq    $48, %rdi       # lägg till basen för tal, 48 (Ascii)
+    call    putChar         # kalla putchar
+    decq    %r13            # räknare för antal siffror
+    cmpq    $0, %r13        # kolla om vi tagit alla siffror
+    jne putIntFinal         
 putIntReturn:
-    popq   %R13
-    popq   %R14
-    popq   %R15
+    popq    %r15
+    popq    %r13
     ret
 
-putChar: # DONE->TO TEST
-/*Rutinen ska lägga tecknet c i utbufferten och flytta fram aktuell position i den ett steg.
-Om bufferten blir full när getChar anropas ska ett anrop till outImage göras, så att man
-får en tömd utbuffert att jobba vidare mot.
-Parameter: tecknet som ska läggas i utbufferten (c i texten)
- */
-    call    checkBuffPos
-    leaq    outBuf, %r14    # r10 hold addr of outBuf
+putChar:
+    pushq   %r14
+    call    checkBuffPos    # kolla så att buffern inte är full
+    leaq    outBuf, %r14    # lägg buffern i r14
     addq    outPos, %r14    # flytta fram aktuell position
-    movb    %dil, (%r14)    # put IN1-param in outBuf
+    movb    %dil, (%r14)    # flytta karaktären till bufferten
     incq    outPos          # outPos++
-    ret
-notOutImage:
+    popq    %r14
     ret
 
-getOutPos: # DONE->TO TEST
+getOutPos:
     movq    outPos, %rax    # sätt output-buffertposition  till OUT-register
     ret
 
-setOutPos: # DONE->TO TEST
-    movq    %rdi, outPos     # sätta out-buffertposition till outPos variable.
-    cmpq    $0, %rdi
+setOutPos:
+    movq    %rdi, outPos    # sätta out-buffertposition till outPos variable.
+    cmpq    $0, %rdi        # output min = 0
     jl      setBuffPosZero
-    cmpq    $64, %rdi
+    cmpq    $64, %rdi       # output max = 64
     jg      setBuffPosMax
     ret
 
 setBuffPosZero:
-    movq    $0, %rdi
+    movq    $0, %rdi        # Hjälpfunktion för setoutPos
     ret
 
 setBuffPosMax:
-    movq    $64, %rdi
+    movq    $64, %rdi       # Hjälpfunktion för setoutPos
     ret
 
  checkBuffPos:
-    pushq %rbx
-    movq outPos, %rbx
-    cmpq $64, %rbx
+    pushq   %rbx
+    movq outPos, %rbx       # flytta outpos till rbx
+    cmpq $64, %rbx          # kolla om buffern är full
     jl returnToFunction
-    call outImage
+    call outImage           # skriv ut om buffern är full
 returnToFunction:
-    popq %rbx
+    popq    %rbx
     ret
